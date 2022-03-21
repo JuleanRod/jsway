6c973ac52da1c7a0e5f6d53f9fedfe6a44439305
Julian <92952502+juleanrod@users.noreply.github.com>
2022-03-20 23:55:46 -0700

    handling attributes

--- ch14/countingElements.js
+++ ch14/countingElements.js
@@ -9,3 +9,14 @@ console.log(countElements("p"));              // Should show 4
 console.log(countElements(".adjective"));     // Should show 3
 console.log(countElements("p .adjective"));   // Should show 3
 console.log(countElements("p > .adjective")); // Should show 2
+
+
+// Handling attributes
+function linkInfo(){
+    const link = Array.from(document.querySelectorAll("a"));
+    console.log(link.length);
+    console.log(link[0].href);
+    console.log(link[link.length-1].href);
+}
+
+linkInfo();
