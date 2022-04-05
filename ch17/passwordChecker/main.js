d1bd440b0ccbec99cb580df47f281785ebdc3f00
Julian <92952502+juleanrod@users.noreply.github.com>
2022-04-04 21:40:12 -0700

    finish implementation

--- ch17/passwordChecker/main.js
+++ ch17/passwordChecker/main.js
@@ -1,22 +1,48 @@
-let regex = new RegExp('/\w+/g');
+// Write the JavaScript code that validates the password when the user submits the form. The validation rules are as follow:
+//
+// The two inputted passwords must be identical.
+// The minimal password length is 6 characters.
+// The password must contain at least one digit.
+// The validation result must be shown on the page with an appropriate message in each case.
 
+const numCheck = new RegExp("\\d");
 const formElement = document.querySelector("form");
 
-
-formElement.addEventListener('submit', e => {
-  const password1Element = e.target.elements.password1.value;  
+formElement.addEventListener("submit", (e) => {
+  const password1Element = e.target.elements.password1.value;
   const password2Element = e.target.elements.password2.value;
-  const passwordHelp1Element = document.getElementById('passwordhelp1');
-  const passwordHelp2Element = document.getElementById('passwordhelp2');
-  
-  console.log(password1Element.elements);
-  if(password1Element !== password2Element){
-      console.log(password1Element);
-    passwordHelp2Element.innerHTML = 'Doesn\'t match password1';
-    passwordHelp2Element.style.color = 'red';
+  const passwordHelp1Element = document.getElementById("passwordhelp1");
+  const passwordHelp2Element = document.getElementById("passwordhelp2");
+
+  console.log(password1Element);
+  console.log(password2Element);
+
+  if (password2Element !== password1Element) {
+    passwordHelp2Element.innerHTML = "error: invalid confirmation";
+    passwordHelp2Element.style.color = "red";
+  } else {
+    passwordHelp1Element.innerHTML = "";
+    passwordHelp2Element.innerHTML = "";
+  }
+
+  if (password1Element.length < 6) {
+    passwordHelp2Element.innerHTML = "error: length must be 6 or greater";
+    passwordHelp2Element.style.color = "red";
+    passwordHelp1Element.innerHTML = "error: length must be 6 or greater";
+    passwordHelp1Element.style.color = "red";
+  } else {
+    passwordHelp1Element.innerHTML = "";
+    passwordHelp2Element.innerHTML = "";
+  }
+
+  if (numCheck.test(password1Element) == false) {
+    passwordHelp1Element.innerHTML = "error: missing a number";
+    passwordHelp1Element.style.color = "red";
+  } else {
+    const passwordHelp = document.getElementById("passwordHelp");
+    passwordHelp.innerHTML = "Signing in ...";
+    passwordHelp.style.color = "lightgreen";
   }
 
   e.preventDefault();
-})
-
-
+});
