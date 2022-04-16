d0876a4627237d37a6a7d81b4b8affe4a55b3953
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-04-15 20:36:40 -0700

    add horizontal movement

--- ch18/bouncing-ball/script.js
+++ ch18/bouncing-ball/script.js
@@ -20,6 +20,33 @@ function rotateAnimation(id, speed) {
 //and also enable the stop button
 document.getElementById('start').addEventListener('click', e => {
     const stop = document.getElementById('stop');
+    moveBall();
     stop.removeAttribute('disabled');
     rotateAnimation('ball', 4);
 });
+
+
+//movement
+const ballElement = document.getElementById('ball');
+const frame = document.getElementById('frame');
+const ballWidth = parseFloat(getComputedStyle(ballElement).width);
+
+// movement value in pixels
+const movement = 2;
+const moveBall = () => {
+    //convert the left position og the ball (value of the form 'XXpx') to a number
+    const xBall =  parseFloat(getComputedStyle(ballElement).left);
+    const max = parseFloat(getComputedStyle(frame).width);
+    // If the ball isn't already to the end of the frame
+    if(xBall + ballWidth <= max) {
+        //ball movement
+        ballElement.style.left = (xBall + movement) + 'px';
+        animationId = requestAnimationFrame(moveBall);
+        requestAnimationFrame(moveBall);
+    } else {
+        // cancel the animation
+        cancelAnimationFrame(animationId);
+    }
+}
+
+let animationId = requestAnimationFrame(moveBall);
