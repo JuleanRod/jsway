8bbdfcdda18f172d0f10a78c5f3fbac5ab54f4e8
Julian <92952502+juleanrod@users.noreply.github.com>
2022-04-19 23:31:20 -0700

    add bouncing animation

--- ch18/bouncing-ball/script.js
+++ ch18/bouncing-ball/script.js
@@ -19,7 +19,6 @@ function rotateAnimation(id, speed) {
 //once start button is clicked, call the rotate animation
 //and also enable the stop button
 document.getElementById('start').addEventListener('click', start);
-document.getElementById('stop').addEventListener('click', stop);
 
 
 function start() {
